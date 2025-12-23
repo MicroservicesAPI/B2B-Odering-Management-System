e7d6e6d381158f8a64187754fb732fe37845de3a
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-12-23 12:25:02 +0100

    implement authenticate endpoints ( loging/register)

--- services/auth-service/src/app/routes.py
+++ services/auth-service/src/app/routes.py
@@ -1,20 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
+from app.db import get_db
 from app.schemas import LoginRequest, RegisterRequest
+from app.service import AuthService
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@auth_router.post("/login")
-async def login(request: LoginRequest):
-    return {"message": "Login successful"}
-
-
 @auth_router.post("/register")
-async def register(request: RegisterRequest):
-    return {"message": "Registration successful"}
+def register(request: RegisterRequest, db: Session = Depends(get_db)):
+    try:
+        user = AuthService.register(db, request)
+        return user
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
-@auth_router.get("/refresh_token")
-def refresh_token():
-    return {"message": "Refresh token successful"}
+@auth_router.post("/login")
+def login(request: LoginRequest, db: Session = Depends(get_db)):
+    try:
+        return AuthService.login(db, request)
+    except ValueError as e:
+        raise HTTPException(status_code=401, detail=str(e))
