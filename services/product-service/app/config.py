bf9d7e80150c8a508111b6882085d5fac8c71f13
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-12-28 16:31:18 +0100

    add config file

---
+++ services/product-service/app/config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    PROJECT_NAME = os.getenv("PROJECT_NAME", "Product service of the B2B ordering system")
+    VERSION = os.getenv("VERSION", "0.1.0")
+
+
+class LocalRunConfig(Config):
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./test_product.db"
+
+
+class ProdRunConfig(Config):
+    SQLALCHEMY_DATABASE_URL = os.getenv("PROD_DATABASE_URL")
+
+
+# will be use depending on the run-context
+app_config = Config()
+local_run_config = LocalRunConfig()
+prod_run_config = ProdRunConfig()
